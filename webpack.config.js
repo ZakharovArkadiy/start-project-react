0027227e704e83ecd827011963ee911ec44f1f44
ZakharovArkadiy <zak.arkadiy@gmail.com>
2017-12-06 03:50:03 +0500

    добавлен clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 // WEBPACK CONFIG FILE
 "use strict";
 
-const webpack           = require('webpack');
-const merge             = require('webpack-merge');
-const path              = require('path');
+const webpack            = require('webpack');
+const merge              = require('webpack-merge');
+const path               = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const PATHS = {
   source: path.join(__dirname, 'source'),
   public: path.join(__dirname, 'public')
 };
 
+// Basic settings webpack
 const CONFIG = (env) => {
 
   const NODE_ENV = (env.production) ? "production" : "development";
@@ -20,15 +22,18 @@ const CONFIG = (env) => {
     },
     output: {
       path: PATHS.public,
-      filename: '[name].js'
+      filename: '[name].[chunkhash].js'
     },
     plugins: [
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
-      })
+      }),
+      new CleanWebpackPlugin(
+        ["public"]
+      )
     ]
   };
 
-};
+}; // end of basic settings webpack
 
 module.exports = CONFIG;
